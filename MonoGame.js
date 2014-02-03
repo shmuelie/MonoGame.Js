9570d02cf480cf37d29de74adbfd78e89ed3b59c
Shmueli Englard <shmueli.yosef@englard.net>
2014-02-02 19:30:18 -0500

    Added MathHelper

--- MonoGame.js
+++ MonoGame.js
@@ -35,6 +35,177 @@
 		}
 	}
 
+	//#region MathHelper
+
+	MathHelper = {};
+
+	if (Object.defineProperty)
+	{
+		Object.defineProperty(MathHelper, "E", {
+			value: Math.E,
+			writable: false,
+			enumerable: true
+		});
+
+		Object.defineProperty(MathHelper, "Log10E", {
+			value: 0.4342945,
+			writable: false,
+			enumerable: true
+		});
+
+		Object.defineProperty(MathHelper, "Log2E", {
+			value: 1.442695,
+			writable: false,
+			enumerable: true
+		});
+
+		Object.defineProperty(MathHelper, "Pi", {
+			value: Math.PI,
+			writable: false,
+			enumerable: true
+		});
+
+		Object.defineProperty(MathHelper, "PiOver2", {
+			value: Math.PI / 2,
+			writable: false,
+			enumerable: true
+		});
+
+		Object.defineProperty(MathHelper, "PiOver4", {
+			value: Math.PI / 4,
+			writable: false,
+			enumerable: true
+		});
+
+		Object.defineProperty(MathHelper, "TwoPi", {
+			value: 2 * Math.PI,
+			writable: false,
+			enumerable: true
+		});
+	}
+	else
+	{
+		MathHelper.E = Math.E;
+		MathHelper.Log10E = 0.4342945;
+		MathHelper.Log2E = 1.442695;
+		MathHelper.Pi = Math.PI;
+		MathHelper.PiOver2 = Math.PI / 2;
+		MathHelper.PiOver4 = Math.PI / 4;
+		MathHelper.TwoPi = Math.PI * 2;
+	}
+
+	MathHelper.barycentric = function (value1, value2, value3, amount1, amount2)
+	{
+		return value1 + (value2 - value1) * amount1 + (value3 - value1) * amount2;
+	};
+
+	MathHelper.catmullRom = function (value1, value2, value3, value4, amount)
+	{
+		var amountSquared = amount * amount;
+		var amountCubed = amountSquared * amount;
+		return (0.5 * (2.0 * value2 + (value3 - value1) * amount + (2.0 * value1 - 5.0 * value2 + 4.0 * value3 - value4) * amountSquared + (3.0 * value2 - value1 - 3.0 * value3 + value4) * amountCubed));
+	};
+
+	MathHelper.clamp = function (value, min, max)
+	{
+	    // First we check to see if we're greater than the max
+	    value = (value > max) ? max : value;
+
+	    // Then we check to see if we're less than the min.
+	    value = (value < min) ? min : value;
+
+	    // There's no check to see if min > max.
+	    return value;
+	};
+
+	MathHelper.distance = function (value1, value2)
+	{
+	    return Math.abs(value1 - value2);
+	};
+
+	MathHelper.hermite = function(value1, tangent1, value2, tangent2, amount)
+	{
+	    var v1 = value1, v2 = value2, t1 = tangent1, t2 = tangent2, s = amount, result;
+	    var sCubed = s * s * s;
+	    var sSquared = s * s;
+
+	    if (amount === 0)
+	    {
+	        result = value1;
+	    }
+	    else if (amount === 1)
+	    {	
+	        result = value2;
+	    }
+	    else
+	    {
+	        result = (2 * v1 - 2 * v2 + t2 + t1) * sCubed + (3 * v2 - 3 * v1 - 2 * t1 - t2) * sSquared + t1 * s + v1;
+	    }
+	    return result;
+	};
+
+	MathHelper.lerp = function (value1, value2, amount)
+	{
+	    return value1 + (value2 - value1) * amount;
+	};
+
+	MathHelper.max = function (value1, value2)
+	{
+	    return Math.max(value1, value2);
+	};
+
+	MathHelper.min = function (value1, value2)
+	{
+	    return Math.min(value1, value2);
+	};
+
+	MathHelper.smoothStep = function (value1, value2, amount)
+	{
+	    var result = MathHelper.clamp(amount, 0, 1);
+	    result = MathHelper.hermite(value1, 0, value2, 0, result);
+
+	    return result;
+	};
+
+	MathHelper.toDegrees = function (radians)
+	{
+	    return radians * 57.295779513082320876798154814105;
+	};
+
+	MathHelper.toRadians = function (degrees)
+	{
+	    return degrees * 0.017453292519943295769236907684886;
+	};
+
+	MathHelper.iEEERemainder = function (x, y)
+	{
+	    return x - (y * Math.round(x / y));
+	};
+
+	MathHelper.wrapAngle = function (angle)
+	{
+	    angle = MathHelper.iEEERemainder(angle, 6.2831854820251465);
+	    if (angle <= -3.14159274)
+	    {
+	        angle += 6.28318548;
+	    }
+	    else
+	    {
+		    if (angle > 3.14159274)
+	        {
+		        angle -= 6.28318548;
+	        }
+	    }
+		return angle;
+	};
+
+	MathHelper.isPowerOfTwo = function (value)
+	{
+	    return (value > 0) && ((value & (value - 1)) === 0);
+	};
+
+	//#endregion
+
 	//#region Point
 
 	var Point_zeroPoint = new Point(0, 0);
